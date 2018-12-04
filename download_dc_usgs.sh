3895806c7550b1ed5503eb0fc04cb4f981f9bf2c
Anthony Li <antli.inbox@gmail.com>
2018-12-04 01:10:53 -0500

    Update file download URLs

--- download_dc_usgs.sh
+++ download_dc_usgs.sh
@@ -1,2 +1,4 @@
 wget https://s3.amazonaws.com/gsvdlbucket/USGS_NED_13_n39w077_IMG.img
-wget https://s3.amazonaws.com/gsvdlbucket/USGS_NED_13_n43w090_IMG.img
+wget https://s3.amazonaws.com/gsvdlbucket/USGS_NED_13_n39w078_IMG.img
+wget https://s3.amazonaws.com/gsvdlbucket/USGS_NED_13_n40w077_IMG.img
+wget https://s3.amazonaws.com/gsvdlbucket/USGS_NED_13_n40w078_IMG.img
